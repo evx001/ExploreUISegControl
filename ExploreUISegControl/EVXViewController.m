47fca7f178ec76710a6473e3b5b33d5f824bb977
evx001 <eviexeris@gmail.com>
2014-06-03 17:28:59 -0400

    commented out the switch to check IBOutlet

--- ExploreUISegControl/EVXViewController.m
+++ ExploreUISegControl/EVXViewController.m
@@ -17,21 +17,21 @@
 - (IBAction)segCntrlValChange:(id)sender {
     // valureChanged is the expected default
     // the control is being passed in through the sender property
-    UISegmentedControl *ctrl = (UISegmentedControl *)sender;
-    // we can use a switch statement for interp of values
-
-    switch([ctrl selectedSegmentIndex]) {
-        case 0:
-            self.segLabelOutput.text = @"Seg1";
-            break;
-
-        case 1:
-            self.segLabelOutput.text = @"Seg2";
-            break;
-        case 2:
-            self.segLabelOutput.text = @"Seg3";
-            break;
-    }
+//    UISegmentedControl *ctrl = (UISegmentedControl *)sender;
+//    // we can use a switch statement for interp of values
+//
+//    switch([ctrl selectedSegmentIndex]) {
+//        case 0:
+//            self.segLabelOutput.text = @"Seg1";
+//            break;
+//
+//        case 1:
+//            self.segLabelOutput.text = @"Seg2";
+//            break;
+//        case 2:
+//            self.segLabelOutput.text = @"Seg3";
+//            break;
+//    }
 
 }
 
